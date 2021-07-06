4634418bc77fb39d387086ee6b0c6ef66b7c39a0
Juan Carlos Marchant <juan.marchant@gmail.com>
2021-07-05 21:32:17 -0400

    add scrollbar package npm

--- app/Controllers/PagesController.php
+++ app/Controllers/PagesController.php
@@ -16,6 +16,7 @@ class PagesController extends Controller
 
     public function responsive()
     {
+        session('success', 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi blanditiis, accusantium eius molestias nisi debitis ut inventore quam placeat, quaerat vero vitae ratione saepe minus ea? In consequatur sunt hic?');
         $this->render('pages.responsive');
     }
 }
